391287b4a741d2d3e6c85b93fc664b60c35315ff
Doctor Cobweb <spinninghalf@gmail.com>
2014-03-04 15:59:37 +1100

    Initial Commit

---
+++ ToDoList/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  ToDoList
+//
+//  Created by andre on 4/03/2014.
+//  Copyright (c) 2014 andre trosky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
